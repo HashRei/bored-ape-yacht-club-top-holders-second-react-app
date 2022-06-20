dbdea58cb7d96270b266122fab3b7719e33d6ad9
HashRei <hashrei@protonmail.com>
2022-06-20 09:51:56 +0200

    Add token query of my bored ape subgraph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createClient } from "urql";
+import { useEffect, useState } from "react";
+
+const APIURL =
+  "https://api.thegraph.com/subgraphs/name/hashrei/bored-ape-yacht-club-top-holders-second";
+
+const query = `
+  query {
+    tokens(first: 30) {
+      image
+      owner{
+        id
+        tokens {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const client = createClient({
+  url: APIURL,
+});
 
 function App() {
+  const [tokens, setTokens] = useState([]);
+  // const [nftListLength, setNftListLength] = useState(5);
+  const nftListLength = 5;
+  useEffect(() => {
+    fetchData();
+  }, []);
+  async function fetchData() {
+    const response = await client.query(query).toPromise();
+    setTokens(response.data.tokens);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table className="App" border="1">
+      {tokens.map((token, index) => (
+        <tr >
+          <p>
+            <b>Holder address:</b> {token.owner.id}
+          </p>
+          <td style={{ flex: "right" }}>
+            <p>Number of Bored Apes: {token.owner.tokens.length}</p>
+            {token.owner.tokens.map((nfts, index) => (
+              <>{index <= nftListLength - 1 && <p>NFT # {nfts.id}</p>}</>
+            ))}
+            {token.owner.tokens.length > nftListLength - 1 && <p>...</p>}
+          </td>
+          {/* {<button onClick={ setNftListLength(10)}>...</button>} */}
+
+          {/* <a href={token.image} target="_blank" rel="noreferrer">IMAGE</a> */}
+          {console.log("id:", token.owner.id)}
+          {console.log("tokens:", token.owner.tokens)}
+        </tr>
+      ))}
+    </table>
   );
 }
 
