b571bd4e2c0e5cbd41ae788b937b626db9f48e76
HashRei <hashrei@protonmail.com>
2022-06-21 17:01:04 +0200

    Add boredApeQuery function with nftListLength

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { createClient } from "urql";
 import { useEffect, useMemo, useState } from "react";
 import { gql } from "graphql-request";
 import { request } from "graphql-request";
@@ -10,9 +9,10 @@ const APIURL =
 const HTTP_GRAPHQL_ENDPOINT =
   "https://api.thegraph.com/subgraphs/name/ensdomains/ens";
 
-const query = `
-  query {
-    tokens(first: 30) {
+function boredApeQuery(numberOfTokens: number) {
+  return gql`
+    {
+      tokens(first: ${numberOfTokens}) {
       owner{
         id
         tokens {
@@ -20,10 +20,10 @@ const query = `
         }
       }
     }
-  }
-`;
+  }`;
+}
 
-function imageQuery(nftID: String) {
+function imageQuery(nftID: string) {
   return gql`
     {
       tokens(where: {tokenID: "${nftID}"}) {
@@ -32,7 +32,7 @@ function imageQuery(nftID: String) {
   }`;
 }
 
-function getQueryENSFromETHAddress(ethAddress: String) {
+function getQueryENSFromETHAddress(ethAddress: string) {
   return gql`
     {
       domains(first: 1, where:{owner:"${ethAddress}"}) {
@@ -45,40 +45,43 @@ function getQueryENSFromETHAddress(ethAddress: String) {
   }`;
 }
 
-const client = createClient({
-  url: APIURL,
-});
-
 function App() {
+  const minimumNftListLength = 5;
+
   const [tokens, setTokens] = useState([]);
   const [ensName, setEnsName] = useState("");
   const [ethAddress, setEthAddress] = useState("");
   const [nftID, setNftID] = useState("0"); // There shouldn't be a nftID 0 in the collection
   const [nftImage, setNftImage] = useState("");
-
-  const nftListLength = 5;
+  const [numberOfTokens, setNumberOfTokens] = useState("30");
+  const [nftListLength, setNftListLength] = useState(minimumNftListLength);
 
   useEffect(() => {
-    fetchData(ethAddress);
-  }, [ethAddress]);
+    fetchData();
+  }, [ethAddress, numberOfTokens]);
 
   useMemo(() => {
     fetchNftImageData();
   }, [nftID]);
 
-  async function fetchData(ethAddress: String) {
-    const response = await client.query(query).toPromise();
-    setTokens(response.data.tokens);
+  async function fetchData() {
+    if (numberOfTokens !== "0") {
+      const nftListResult = await request(
+        APIURL,
+        boredApeQuery(parseInt(numberOfTokens))
+      );
+      setTokens(nftListResult.tokens);
+    } else setTokens([]);
 
     if (ethAddress.length === 42) {
-      const result = await request(
+      const ethAdrressResult = await request(
         HTTP_GRAPHQL_ENDPOINT,
         getQueryENSFromETHAddress(ethAddress)
       );
-      setEnsName(result.domains[0].name);
-    } else {
-      setEnsName("No ENS");
-    }
+      if (ethAdrressResult.domains[0] === undefined)
+        setEnsName("No ENS for this address");
+      else setEnsName(ethAdrressResult.domains[0].name);
+    } else setEnsName("Not an ETH address");
   }
 
   async function fetchNftImageData() {
@@ -88,6 +91,14 @@ function App() {
 
   return (
     <div className="App">
+      <p>Number of Bored ape to list</p>
+      <input
+        type="number"
+        min={0}
+        style={{ width: "400px" }}
+        placeholder="Enter number of listed Apes..."
+        onChange={(event) => setNumberOfTokens(event.target.value)}
+      />
       <p>ENS tracker</p>
       <input
         style={{ width: "400px" }}
@@ -112,14 +123,37 @@ function App() {
       {tokens.map((token: any, index) => (
         <div key={index}>
           <b>Holder address:</b> {token.owner.id}
+          {console.log("token", token)}
           <div style={{ flex: "right" }}>
             <p>Number of Bored Apes: {token.owner.tokens.length}</p>
             {token.owner.tokens.map((nfts, indexSecond: number) => (
               <div key={nfts.id}>
-                {indexSecond <= nftListLength - 1 && <p>NFT # {nfts.id}</p>}
+                <>
+                  {() => setNftListLength(minimumNftListLength)}
+                  {indexSecond <= nftListLength - 1 && <p>NFT # {nfts.id}</p>}
+                </>
               </div>
             ))}
-            {token.owner.tokens.length > nftListLength - 1 && <p>...</p>}
+            {token.owner.tokens.length > nftListLength - 1 &&
+              nftListLength <= minimumNftListLength && (
+                <button
+                  onClick={() => {
+                    setNftListLength(parseInt(token.owner.tokens.length));
+                  }}
+                >
+                  ...
+                </button>
+              )}
+            {nftListLength > minimumNftListLength &&
+              token.owner.tokens.length > minimumNftListLength && (
+                <button
+                  onClick={() => {
+                    setNftListLength(minimumNftListLength);
+                  }}
+                >
+                  ...
+                </button>
+              )}
           </div>
         </div>
       ))}
